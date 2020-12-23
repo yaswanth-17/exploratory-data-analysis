2309f0f369fcf65c388fab7c143efffa093f4ceb
yaswanth-17 <60813459+yaswanth-17@users.noreply.github.com>
2020-12-23 10:30:38 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,47 +1,33 @@
-# libraries
-require(utils)
-require(cairoDevice) # anti-aliasing figure
-# download, load data and subset
-fileUrl <-
-'https://d396qusza40orc.cloudfront.net/exdata%2Fdata
-%2FNEI_data.zip'
-download.file(fileUrl,
-destfile = './Data.zip',
-method = 'curl', quiet = T)
-if(file.exists('./Data.zip')) {
-# Extract data file
-unzip('./Data.zip')
-# Delete original Zip file if it exists
-invisible(file.remove('./Data.zip'))
-}
-## This first line will likely take a few seconds. Be
-patient!
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
-# Delete data files .rds
-if(all(file.exists('./summarySCC_PM25.rds',
-'Source_Classification_Code.rds'))) {
-invisible(file.remove('./summarySCC_PM25.rds',
-'Source_Classification_Code.rds'))) {
-invisible(file.remove('./summarySCC_PM25.rds',
-'Source_Classification_Code.rds'))
-}
-# Create the png
-png('./plot4.png', width = 700, height = 370,
-res = 75, type = 'cairo') # default is 480px X 480px
-# merge data and make Plot
-NEI %>%
-inner_join(SCC, by = 'SCC') %>%
-dplyr::filter(str_detect(Short.Name, '[Cc]oal')) %>%
-group_by(year, type) %>%
-summarise(sum = sum(Emissions)) %>%
-ggplot(aes(year, sum)) +
-geom_point() + geom_line() +
-facet_wrap(~type, ncol = 4) +
-labs(title = 'Total PM2.5 Coal Emission by Year',
-subtitle = 'Subsetted by Type of Source') +
-xlab('') + ylab('Total PM2.5 Emission (tons)') +
-scale_x_continuous(breaks = unique(NEI$year)) +
-theme_bw()
-# Close png file
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+power <- read.table(unz(temp,"household_power_consumption.txt"), 
+                    sep=";", 
+                    header = T, 
+                    na="?", 
+                    colClasses = c("character",
+                                   'character',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric'))
+
+unlink(temp)
+power <- power[which(power$Date == '2/2/2007' | power$Date=='1/2/2007'),]
+
+power$POSIX <-as.POSIXlt.character(paste(power$Date,power$Time),format = "%d/%m/%Y %H:%M:%S")
+
+
+#plot4
+png(filename="plot4.png",width=480, height=480)
+par(mfrow=c(2,2))
+plot(x=power$POSIX ,y=power$Global_active_power, type = 'l', xlab='',ylab = 'Global Active Power')
+plot(x=power$POSIX ,y=power$Voltage, type = 'l', xlab='datetime',ylab = 'Voltage')
+plot(x=power$POSIX,y=power$Sub_metering_1, type='l', col = 'black', ylab = 'Energy sub metering', xlab = '')
+lines(x=power$POSIX,y=power$Sub_metering_2, col='red')
+lines(x=power$POSIX,y=power$Sub_metering_3, col='blue')
+legend('topright', legend = c('Sub_metering_1',"Sub_metering_2","Sub_metering_3"), col = c('black','red','blue'), lty = 1, bty = "n")
+plot(x=power$POSIX ,y=power$Global_reactive_power, type = 'l', xlab='datetime',ylab = 'Global_reactive_power')
 dev.off()
